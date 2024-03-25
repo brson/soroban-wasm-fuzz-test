f7eeb0b8e48830054da6c774182b524496c947ee
Brian Anderson <andersrb@gmail.com>
2024-03-25 13:23:39 +0100

    .

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,7 +1,24 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
+use soroban_sdk::*;
+use soroban_sdk::testutils::arbitrary::arbitrary::Arbitrary;
 
-fuzz_target!(|data: &[u8]| {
-    // fuzzed code goes here
+fuzz_target!(|input: Input| {
+    run(input);
 });
+
+mod example_token {
+    soroban_sdk::contract_import!(file = "../../example_token.wasm");
+}
+
+#[derive(Arbitrary, Default)]
+struct Input {
+    amount: i128,
+}
+
+fn run(input: Input) {
+    let env = Env::default();
+
+    env.register_contract_wasm(None, example_token::WASM);
+}
