e60cded6af42c54003c46abc6e6b0932f862415e
Brian Anderson <andersrb@gmail.com>
2024-03-25 13:38:05 +0100

    .

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -2,23 +2,38 @@
 
 use libfuzzer_sys::fuzz_target;
 use soroban_sdk::*;
-use soroban_sdk::testutils::arbitrary::arbitrary::Arbitrary;
+use soroban_sdk::testutils::arbitrary::Arbitrary;
+use soroban_sdk::testutils::arbitrary::arbitrary;
+use soroban_sdk::testutils::Address as _;
 
 fuzz_target!(|input: Input| {
     run(input);
 });
 
 mod example_token {
-    soroban_sdk::contract_import!(file = "../../example_token.wasm");
+    soroban_sdk::contractimport!(file = "../soroban_token_contract.wasm");
 }
 
-#[derive(Arbitrary, Default)]
+#[derive(Arbitrary, Debug)]
 struct Input {
-    amount: i128,
+    amount: u32,
 }
 
 fn run(input: Input) {
     let env = Env::default();
 
-    env.register_contract_wasm(None, example_token::WASM);
+    env.mock_all_auths();
+    env.budget().reset_unlimited();
+
+    let admin_addr = Address::generate(&env);
+    let token_addr = env.register_contract_wasm(None, example_token::WASM);
+
+    let token_client = example_token::Client::new(&env, &token_addr);
+
+    token_client.initialize(
+        &admin_addr,
+        &input.amount,
+        &String::from_str(&env, "Token"),
+        &String::from_str(&env, "TKN"),
+    );
 }
